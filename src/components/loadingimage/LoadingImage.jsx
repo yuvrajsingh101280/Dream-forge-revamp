3d9d52640711225a73f4f631e969df956dd550fe
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-07-15 21:14:54 +0530

    laoding

--- src/components/loadingimage/LoadingImage.jsx
+++ src/components/loadingimage/LoadingImage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import loadingImage from "../../assets/images/LoadingImage.png";
+import loadingImage from "../../assets/images/LoadingPage.png";
 const LoadingImage = () => {
   return (
     <div className="w-[1512px] h-[861px]">
-      <img src={LoadingImage} alt="" />
+      <img src={loadingImage} alt="" />
     </div>
   );
 };
