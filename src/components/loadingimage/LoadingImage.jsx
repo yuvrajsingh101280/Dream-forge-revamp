02d07315fcdcd6d332abfc40fbcd58f6cb953d78
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-07-15 21:06:12 +0530

    fixed the image problem

--- src/components/loadingimage/LoadingImage.jsx
+++ src/components/loadingimage/LoadingImage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import loadingImage from "../../assets/images/LoadingImage.png";
 const LoadingImage = () => {
   return (
     <div className="w-[1512px] h-[861px]">
-      <img src="/src/assets/images/LoadingPage.png" alt="" />
+      <img src={LoadingImage} alt="" />
     </div>
   );
 };
